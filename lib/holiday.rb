39880f461f436db92e044e83a0f3fff0debeb847
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-23 16:28:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,10 +76,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+    bbq_array = [] 
   holiday_hash.collect do |season, holiday|
     holiday.collect do |data, supplies|
       if supplies.include?("BBQ")
-        return data
+        
       end
     end
   end
